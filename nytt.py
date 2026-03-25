30b949d6e03fc79843dfd602663698e886815bf3
Dagrate123 <sigia002@osloskolen.no>
2026-03-25 22:49:43 +0100

    database + notes nesten ferdig

--- nytt.py
+++ nytt.py
@@ -1,5 +1,9 @@
 import requests 
 import FreeSimpleGUI as sg
+import sqlite3
+
+server = sqlite3.connect('server.db')
+cursor = server.cursor()
 
 sidebar = [
     [sg.Text("Notes", font=("Any", 12, "bold"))],
@@ -9,7 +13,7 @@ sidebar = [
         key="-NAV-",
         enable_events=True
     )],
-    [sg.Button('Exit')],
+    [sg.Button('Save')], [sg.Button('Exit')],
 ]
 
 content = [
@@ -32,8 +36,43 @@ layout = [
 window = sg.Window('Fullscreen', layout, finalize=True)
 window.maximize()
 
+cursor = server.cursor()
+server.execute("SELECT id, notename FROM notes WHERE user_id = ?", (1, )),
+notater = cursor.fetchall()
+notat_dictionary = {name: note_id for note_id, name in notater}
+
+window["-NAV-"].update(values=list(notat_dictionary.keys()))
+
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
+    if event == "-NAV-":
+        selected = values["-NAV-"]
+        if selected:
+            note_name = selected[0]
+            note_id = notat_dictionary[note_name]
+
+            cursor.execute("SELECT contents FROM notater WHERE id = ?", (note_id, ))
+            content = cursor.fetchone()[0]
+
+            window["-CONTENT-"].update(value=content)
+
+    if event == Save:
+        selected = values["-NAV-"]
+        if selected:
+            note_name = selected[0]
+            note_id = notat_dictionary[note_name]
+            new_content = values["-CONTENT-"]
+
+            cursor.execute(
+                """
+                UPDATE notater
+                SET contents = ?, updated_at = CURRENT_TIMESTAMP,
+                WHERE id = ? 
+                """, (new_content, note_id)
+            )
+
+            server.commit()
+
 window.close()
