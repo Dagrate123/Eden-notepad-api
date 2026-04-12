295a2f816ac15b2c9addb9a878d1e67306e27778
Dagrate123 <sigia002@osloskolen.no>
2026-04-12 21:54:18 +0200

    delete + update + bug fixes

--- server.py
+++ server.py
@@ -13,19 +13,54 @@ class Handler(BaseHTTPRequestHandler):
             rows = cursor.fetchall()
 
             self.send_response(200)
+            self.send_header("Content-Type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps(rows).encode())
 
+    def do_POST(self):
+
+        length = int(self.headers.get("Content-Length", 0))
+        body = self.rfile.read(length)
+
+        try:
+            data = json.loads(body)
+        except:
+            data = None
+
+        if self.path == "/make-note":
+            cursor.execute(
+                "INSERT INTO notes (title, content) VALUES (?, ?)",
+                (data, "")
+            )
+            conn.commit()
+
+            self.send_response(200)
+            self.end_headers()
+
+        elif self.path == "/update":
+            for n in data:
+                cursor.execute("""
+                    UPDATE notes
+                    SET content = ?
+                    WHERE id = ?
+                """, (n["content"], n["id"]))
+
+            conn.commit()
+
+            self.send_response(200)
+            self.end_headers()
+
+        elif self.path == "/delete":
+            cursor.execute(
+                "DELETE FROM notes WHERE id = ?",
+                (data,)
+            )
+            conn.commit()
+
+            self.send_response(200)
+            self.end_headers()
+
+
 server = HTTPServer(("0.0.0.0", 8000), Handler)
+print("Server running on port 8000")
 server.serve_forever()
-
-def do_POST(self):
-    if self.path == "/make-note":
-        length = int(self.headers["Content-Length"])
-        data = json.loads(self.rfile.read(length))
-
-        cursor.execute("INSERT INTO notes (title, content) VALUES (?,?)", (data, ""))
-        conn.commit()
-
-        self.send_response(200)
-        self.end_headers()
